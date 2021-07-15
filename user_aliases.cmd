df1f780ee1926e21c34d2a6829f297bce2858b53
Sebastian Vargr <870110+sebbdk@users.noreply.github.com>
2021-07-15 12:19:42 +0200

    added aliases

---
+++ user_aliases.cmd
@@ -0,0 +1,20 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+gst=git status $*q
+gct=cls & git status $*
+gco=git checkout $*
+gls=git log --oneline
+gbr=git branch --sort=-committerdate | head -n 10
